d5b23078f5bb2a740de40d126d2ed7094ae2b542
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-12 18:21:37 -0300

    some updates

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,27 +1,6 @@
 import { createRoot} from "react-dom";
 import Pet from "./Pet";
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Doink",
-//       animal: "Cat",
-//       breed: "Mixed",
-//     }),
-//   ]);
-// };
-
 const App = () => {
   return (
     <div>
