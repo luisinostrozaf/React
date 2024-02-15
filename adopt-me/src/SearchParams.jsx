7aac9380e858ffa3061948bb58b60645ca4f9528
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-14 23:39:55 -0300

    Update SearchParams.jsx

--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -5,6 +5,8 @@ const Animal = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
     const [location, setLocation] = useState("");
     const [animal, setAnimal] = useState("");
+    const [breed, setBreed] = useState("");
+    const breeds = [""]
     return (
         <div className="search-params">
             <form>
@@ -12,11 +14,10 @@ const SearchParams = () => {
                     Location
                     <input onChange={e => setLocation(e.target.value)}id="location" value={location} placeholder="Location" />
                 </label>
-                <button>Submit</button>
                 <label htmlFor="animal">
                     Animal
                 </label>
-                <select value={animal} onChange={e => setAnimal(e.target.value)}>
+                <select value={animal} onChange={e => {setAnimal(e.target.value); setBreed("");}}>
                     <option />
                     {Animal.map(animal => (
                         <option key={animal} value={animal}>
@@ -24,6 +25,18 @@ const SearchParams = () => {
                         </option>
                     ))}
                 </select>
+                <label htmlFor="breed">
+                    Breed
+                </label>
+                <select value={breed} disabled = {breeds.length === 0} onChange={e => setBreed(e.target.value)}>
+                    <option />
+                    {breeds.map(breed => (
+                        <option key={breed} value={breed}>
+                            {breed}
+                        </option>
+                    ))}
+                </select>
+                <button>Submit</button>
             </form>
         </div>
     );
