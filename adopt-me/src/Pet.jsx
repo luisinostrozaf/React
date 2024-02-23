3767d15ee0ff7625983de8c7f70372dc6d2c2b6c
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-22 23:21:39 -0300

    Update Pet.jsx

--- adopt-me/src/Pet.jsx
+++ adopt-me/src/Pet.jsx
@@ -1,6 +1,10 @@
-const Pet = (props) => {
+const Pet = ({name, animal, breed, images, location, id}) => {
+    let hero = 'https://pets-images.dev-apis.com/pets/none.jpg';
+    if images.length) {
+        hero = images[0];
+    }
     return (
-        <div className="pet">
+        <div>
             <h1>{props.name}</h1>
             <h2>{props.animal}</h2>
             <h2>{props.breed}</h2>
