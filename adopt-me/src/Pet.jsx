5da81a3a6e77933183b4b7b8de0cd2df71a7a45c
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-23 18:05:02 -0300

    Update Pet.jsx

--- adopt-me/src/Pet.jsx
+++ adopt-me/src/Pet.jsx
@@ -1,5 +1,5 @@
 const Pet = ({name, animal, breed, images, location, id}) => {
-    let hero = 'https://pets-images.dev-apis.com/pets/none.jpg';
+    let hero = 'https://pets-images.dev-apis.com/pets/none.jpg'; //api de frontendmasters
     if images.length) {
         hero = images[0];
     }
