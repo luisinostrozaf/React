1487545743e2adca6ab6131bc9bef2f8c7d63f25
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-24 20:18:53 -0300

    Update Pet.jsx

--- adopt-me/src/Pet.jsx
+++ adopt-me/src/Pet.jsx
@@ -5,6 +5,13 @@ const Pet = ({name, animal, breed, images, location, id}) => {
     }
     return (
         <a href={`/details/${id}`} className="pet">
+            <div className="image-container">
+                <img src={hero} alt={name} />
+            </div>
+            <div className="info">
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${location}`}</h2>
+            </div>
         </a>
     );
 };
