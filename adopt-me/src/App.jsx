173875b9fd4afbdd68b0893ba0e6c4f2372fd407
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-27 20:01:32 -0300

    Update App.jsx

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,14 +1,16 @@
 import { createRoot} from "react-dom/client";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details"
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+      <h1>Adopt Me!</h1>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
     </BrowserRouter>
   );
 }
