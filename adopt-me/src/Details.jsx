a9bc054c72db390740ada6e12661b0992ca91b2a
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-28 17:17:10 -0300

    Update Details.jsx

--- adopt-me/src/Details.jsx
+++ adopt-me/src/Details.jsx
@@ -1,8 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const Details = () => {
+    const { id } = useParams();
     return (
-        <div>
-            <h1>Details</h1>
-        </div>
+            <h1>{id}</h1>
     )
 };
 
