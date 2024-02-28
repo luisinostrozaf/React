284ebb2720bba7e308c21df4e0082354cd21dd73
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-28 12:41:21 -0300

    added a search bar

--- Recipify/recipify/src/App.jsx
+++ Recipify/recipify/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import SearchBar from './SearchBar.jsx';
 
 function App() {
   return (
     <div>
        <h1>RECIPIFY</h1>
+       <SearchBar />
     </div>
   )
 }
---
+++ Recipify/recipify/src/SearchBar.jsx
@@ -0,0 +1,20 @@
+function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState('');
+  //const history = useHistory();
+
+  const handleSearch = (e) => {
+    e.preventDefault;
+  }
+    return (
+        <form onSubmit={handleSearch}>
+        <input 
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+        </form>
+    )
+}
+
+export default SearchBar;
