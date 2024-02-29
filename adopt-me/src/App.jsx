f872dd63999185dcbdc4440c934ec4a90564f05f
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-02-29 14:50:33 -0300

    Update App.jsx

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,5 +1,6 @@
 import { createRoot} from "react-dom/client";
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details"
 
