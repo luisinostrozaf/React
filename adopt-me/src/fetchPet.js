93cf5b2e0f4e17833ed8ccec85df8f3996ab72c7
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-03-04 12:57:11 -0300

    Update fetchPet.js

--- adopt-me/src/fetchPet.js
+++ adopt-me/src/fetchPet.js
@@ -1,3 +1,11 @@
 const fetchPet = async ({queryKey}) => {
     const id = queryKey[1];
+    const apiRES = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+    if (!apiRES.ok) {
+        throw new Error('details/${id} failed');
+    }
+
+    return apiRES.json();
 };
+
+export default fetchPet;
