3190fed0d12a1ffd81103f654740f3cc7702dbda
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-03-05 14:31:43 -0300

    changes

--- Recipify/recipify/src/Body.jsx
+++ Recipify/recipify/src/Body.jsx
@@ -1,8 +1,9 @@
 function Body () {
     return (
         <div>
-            <h1>AQUI VA EL CUERPO</h1>
-            <button>Button</button>
+            <h1 className="font-mono text-4xl text-blue-500">AQUI VA EL CUERPO</h1>
+            <input type="text" placeholder="Input recipe to find"></input>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded">Button</button>
         </div>
     )
 }
