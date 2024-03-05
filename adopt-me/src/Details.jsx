49898c96d2d353a2d7dac93f1988c8d4ec61b573
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-03-05 13:59:55 -0300

    Update Details.jsx

--- adopt-me/src/Details.jsx
+++ adopt-me/src/Details.jsx
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetch from "./fetch";
 
 const Details = () => {
     const { id } = useParams();
+    const results = useQuery(['details', id], fetch);
     return (
             <h1>{id}</h1>
     )
