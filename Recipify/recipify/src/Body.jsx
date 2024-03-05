3071ea948a77f807e540306dabcd491863c69904
Luis Inostroza <50220590+luisinostrozaf@users.noreply.github.com>
2024-03-05 14:20:02 -0300

    some changes

--- Recipify/recipify/src/Body.jsx
+++ Recipify/recipify/src/Body.jsx
@@ -2,9 +2,7 @@ function Body () {
     return (
         <div>
             <h1>AQUI VA EL CUERPO</h1>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Button
-            </button>
+            <button>Button</button>
         </div>
     )
 }
